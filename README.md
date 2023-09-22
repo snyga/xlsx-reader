ffae8c263c4df365dd7211eea609c368fa0f594c
Simon Nyggard-Thomsen <36227010+snyga@users.noreply.github.com>
2023-09-22 09:02:56 +0200

    Updated readme.md

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# xlsx-reader
- 
+# xlxs_reader
+GUI for excel database 
+
+## How to
+Download edit_status_x.y.exe and place it in the same folder/directory as a xlxsfile with `' ID '` at cell A1. The rest shouldn't matter.
+
+## Requiremnts
+The only requirements are pandas and PySimpleGUI.
+
+## License
+Licensed under MIT 3.0
---
+++ build_with_Pyintstaller.py
@@ -0,0 +1,13 @@
+#%%
+from sys import argv
+from timeit import timeit
+import subprocess
+start = timeit()
+
+pyinstaller_cmd = 'pyinstaller --noconfirm --onefile --console --hidden-import "openpyxl.cell._writer" --collect-submodules "openpyxl"  "C:/Users/SD38JP/OneDrive - Aalborg Universitet/Dokumenter/Basement/xlsx-reader/edit_status_0.2.py"'
+
+
+
+
+
+
